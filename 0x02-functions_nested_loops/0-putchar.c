0a0c42d1980ba14c2e5d89577cfaa173339fe9a5
ashenafi <ashenafibezabeh@gmail.com>
2023-02-21 00:04:02 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int _putchar(char c);
+
+/**
+ * main - Entry point
+ *
+ * Description: using sizeof to print the size of various types.
+ *
+ * Return: Always 0 (Succesful)
+*/
+
+int main(void)
+{
+
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+
+}
